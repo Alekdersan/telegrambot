26a27546d2feed57f4e6ece729fec785e24cda2e
alekdersan <repka1795@gmail.com>
2022-07-22 00:00:41 +0300

    First commit

---
+++ telegrambot/src/test/java/pro/sky/java/coursework5/telegrambot/TelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.coursework5.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
